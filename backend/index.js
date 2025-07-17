9ec8940d6796be332f5ee4762cf3eb336193bcee
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-17 09:17:57 -0300

    Project setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import "dotenv/config"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.get("/",(req,res) => {
+    res.send("Servidor ok")
+})
+
+
+app.listen(PORT,() => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+
