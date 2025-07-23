bfcd7d83a054c884ede9253ac9841260bb46efac
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-23 08:24:23 -0300

    feat(user) implement user profile edit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,22 +1,23 @@
 import User from "../models/user.model.js"
 import '../models/post.model.js'
 import "../models/loop.model.js"
+import uploadOnCloudinary from "../config/upload.js";
 
 
 export const getCurrentUser = async (req, res) => {
 
-    try{
+    try {
 
         const userId = req.userId;
         const user = await User.findById(userId).populate("posts loops").populate("posts loops posts.author posts.comments saved saved.author");
-        if(!user){
+        if (!user) {
             return res.status(404).json({ message: "Usuário não encontrado" })
         }
 
         return res.status(200).json(user);
 
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         return res.status(500).json({ message: `erro ao carregar o usuário ${error}` })
     }
@@ -25,18 +26,67 @@ export const getCurrentUser = async (req, res) => {
 
 export const suggestedUsers = async (req, res) => {
 
-    try{
+    try {
 
         const users = await User.find({
-            _id: { $ne: req.userId } 
+            _id: { $ne: req.userId }
         })
-        .select("-password")
+            .select("-password")
 
         res.status(200).json(users);
 
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({ message: `sugestão de usuários ${error}` });
     }
 
 }
+
+
+export const editProfile = async (req, res) => {
+    try {
+        const { name, userName, bio, profession, gender } = req.body;
+
+
+        const user = await User.findById(req.userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "Usário não encontrado" });
+        }
+
+        const sameUserWithUserName = await User.findOne({ userName }).select(
+            "-password"
+        );
+
+        if (
+            sameUserWithUserName &&
+            sameUserWithUserName._id.toString() !== req.userId
+        ) {
+            return res.status(400).json({ message: "Nome de usuário já existe" });
+        }
+
+        let profileImage = user.profileImage;
+        if (req.file) {
+            profileImage = await uploadOnCloudinary(req.file.path);
+        }
+
+        user.name = name;
+        user.userName = userName;
+        user.bio = bio;
+        user.profession = profession;
+        user.gender = gender;
+        user.profileImage = profileImage;
+
+        await user.save();
+
+        const userResponse = user.toObject();
+        delete userResponse.password;
+
+        return res.status(200).json({
+            message: "Perfil atualizado com sucesso",
+            user: userResponse,
+        })
+        
+    } catch (error) {
+        return res.status(500).json({ message: `Erro ao editar perfil  ${error}` });
+    }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { getCurrentUser, suggestedUsers } from "../controllers/user.controller.js"
-
+import { editProfile, getCurrentUser, suggestedUsers } from "../controllers/user.controller.js"
+import {upload} from "../middleware/multer.js"
 
 const userRoutes = express.Router()
 
 userRoutes.get("/current",isAuth,getCurrentUser)
 userRoutes.get("/suggested",isAuth,suggestedUsers)
+userRoutes.post("/edit-profile",isAuth,upload.single("profileImage"),editProfile)
 
 export default userRoutes
