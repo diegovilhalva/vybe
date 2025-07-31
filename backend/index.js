496eafa70ae1361bff7d6e8ba14a8c260ea89707
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-31 12:19:30 -0300

    fix(backend): add origin to cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000
 
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: process.env.CLIENT_ENDPOINT, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
