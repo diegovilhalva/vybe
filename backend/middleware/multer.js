f9d53c9c32ebbc1825f47efad1904c02446b6663
Diego Vilhalva <diegorocha450@yahoo.com>
2025-08-01 08:29:54 -0300

    fix: fix upload error

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,44 +1,12 @@
 import multer from 'multer';
-import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
-import fs from 'fs'; 
-import os from 'os';
-
-const uploadPath = path.join(os.tmpdir(), 'uploads/temp')
-
-const ensureDirExists = (dirPath) => {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true })
-  }
-};
-
-ensureDirExists(uploadPath)
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    ensureDirExists(uploadPath); 
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, `${uuidv4()}${ext}`);
-  },
+    destination: (req, file, cb) => {
+        cb(null, './uploads/temp');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
 });
 
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype.startsWith("image/") ||
-    file.mimetype.startsWith("video/")
-  ) {
-    cb(null, true);
-  } else {
-    cb(new Error("Tipo de arquivo não suportado"), false);
-  }
-};
-
-export const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 30 * 1024 * 1024, 
-  },
-});
+export const upload = multer({ storage: storage });
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,10 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import {upload} from "../middleware/multer.js"
+import { upload } from "../middleware/multer.js"
 import { comment, getAllPosts, like, saved, uploadPost } from "../controllers/post.controller.js"
 const postRoutes = express.Router()
 
-postRoutes.post("/upload",isAuth,upload.single("media"),uploadPost)
+postRoutes.post("/upload", isAuth, upload.single("media"), uploadPost)
 postRoutes.get("/getAll", isAuth, getAllPosts)
 postRoutes.get("/like/:postId", isAuth, like)
 postRoutes.get("/saved/:postId", isAuth, saved)
