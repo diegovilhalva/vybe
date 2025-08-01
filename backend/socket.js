577dad06e12f6bb148fdb512f67055979db41a59
Diego Vilhalva <diegorocha450@yahoo.com>
2025-08-01 07:56:58 -0300

    fix(backend):  add url to socket config

--- backend/socket.js
+++ backend/socket.js
@@ -1,13 +1,14 @@
 import http from 'http';
 import express from 'express';
 import { Server } from 'socket.io';
+import "dotenv/config"
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.CLIENT_ENDPOINT,
         methods: ["GET", "POST"],
     }
 });
